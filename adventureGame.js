0ad0b93dd3817df71f8ac842d29621bd74dc4358
waltb <waltbailey@ymail.com>
2018-02-11 17:58:04 -0800

    initial commit

---
+++ adventureGame.js
@@ -0,0 +1,116 @@
+// Define the size of the game play area increase by 1
+var maxX = 6
+var maxY = 6
+
+// User starts at x0, y0 (bottom left of board)
+var userX = 0
+var userY = 0
+
+// Hide the treasure, also with x y values
+var treasureX = 2
+var treasureY = 1
+
+
+// Flag that controls loop 
+var treasureFound = false 
+
+// Get user's name
+var name = prompt("Welcome brave adventurer! What are you called?")
+alert("Ok"+ " "+ name + "!" +" "+"Let the adventure begin!");
+
+
+//The start button
+
+document.getElementById("click").onclick = function () { alert('hello!'); };
+
+
+// Randomize the user start location 
+var userX = Math.floor(Math.random() * (maxX + 1))
+var userY = Math.floor(Math.random() * (maxY + 1))
+console.log("You are starting in sector (" + userX + ", " + userY + ")")
+
+
+while(!treasureFound){
+  var direction = prompt("Which direction would you like to go in? (north, south, east, west)")
+  
+  console.log(direction)
+  
+  // tmp vars, only used for checking validity of new user location after move
+  var newX 
+  var newY
+  
+  // When new direction entered:
+  // see what new user location would be
+  // check if new user location is valid
+  // check if new user location is treasure
+  
+  if(direction == "north"){
+    newX = userX 
+    newY = userY + 1
+    // neither value < 0, neither is > max
+    if(newX >= 0 && newY >= 0 && newX <= maxX && newY <= maxY){
+      userX = newX 
+      userY = newY
+      
+    }else {
+     console.log("There is a forbidding mountain range in that direction, you cannot go") 
+    }
+ 
+  }else if(direction == "east"){
+    newX = userX + 1
+    newY = userY 
+    // neither value < 0, neither is > max
+    if(newX >= 0 && newY >= 0 && newX <= maxX && newY <= maxY){
+      userX = newX 
+      userY = newY
+    }else {
+     console.log("There is a forbidding mountain range in that direction, you cannot go") 
+    }
+    
+  }else if(direction == "south"){
+    newX = userX 
+    newY = userY - 1 
+    // neither value < 0, neither is > max
+    if(newX >= 0 && newY >= 0 && newX <= maxX && newY <= maxY){
+      userX = newX 
+      userY = newY
+    }else {
+     console.log("There is a forbidding mountain range in that direction, you cannot go") 
+    }
+    
+  }else if(direction == "west"){
+    newX = userX - 1
+    newY = userY
+    // neither value < 0, neither is > max
+    if(newX >= 0 && newY >= 0 && newX <= maxX && newY <= maxY){
+      userX = newX 
+      userY = newY
+    }else {
+     console.log("There is a forbidding mountain range in that direction, you cannot go") 
+    }
+    
+  }else {
+   console.log("please enter a real direction") 
+  }
+  
+  console.log("The user's x is: " + userX)
+  console.log("The user's y is: " + userY)
+  
+  // see if user location matches treasure
+  if(userX == treasureX && userY == treasureY){
+    console.log("Hooray! You found the treasure!")
+   treasureFound = true 
+  }
+  
+  
+}
+    
+
+
+
+
+
+
+
+
+
